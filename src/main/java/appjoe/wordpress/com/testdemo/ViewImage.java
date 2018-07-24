2916a8a4e3cbe1a9df7634f4798ea04780a21063
Apurv Jyotirmay <apurvj199512@gmail.com>
2018-07-24 13:48:34 +0530

    Made visual changes to image view

--- src/main/java/appjoe/wordpress/com/testdemo/ViewImage.java
+++ src/main/java/appjoe/wordpress/com/testdemo/ViewImage.java
@@ -3,6 +3,8 @@ package appjoe.wordpress.com.testdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -15,6 +17,9 @@ public class ViewImage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_view_image);
         ImageView imageView = findViewById(R.id.fullscreenImage);
 
